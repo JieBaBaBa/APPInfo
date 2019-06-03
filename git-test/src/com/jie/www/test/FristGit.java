12b54f740b628fa3c9c781a83408b031cddb4bd7
Chenxiaojie <913063977@qq.com>
2019-06-03 21:40:01 +0800

    本地工程添加到git

---
+++ git-test/src/com/jie/www/test/FristGit.java
@@ -0,0 +1,11 @@
+package com.jie.www.test;
+
+public class FristGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
