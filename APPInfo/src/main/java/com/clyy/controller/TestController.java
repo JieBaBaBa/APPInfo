b0f7ef6ccd4d8c29582e93bb5c8a2ce3fbd68d07
Chenxiaojie <913063977@qq.com>
2019-06-04 17:29:18 +0800

    第3次提交---环境搭建

---
+++ APPInfo/src/main/java/com/clyy/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.clyy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test.html")
+    public String test(){
+        return "chartjs";
+    }
+}
